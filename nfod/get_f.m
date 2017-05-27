64d05b5c2abbcd32156f63d38ec0a0567b9df30d
seacozz2007 <84089842@qq.com>
2017-05-27 12:47:38 +0800

    modify the test data. err res

--- nfod/get_f.m
+++ nfod/get_f.m
@@ -1,23 +1,23 @@
 function [ fd ] = get_f( wt,dsp,vel)
 %获取所受的力 包括 不平衡力Fu 激励力Ft 和 轴承力Fb
 %wt 当前转角,z 为位移
-    global gP gQ;
+    global gP gQ gN;
 
-    fd = zeros(2,1);
+    fd = zeros(gN,1);
     fu = fd;
     ft = fd;
     fb = fd;
     
     %不平衡力
     fu(1) = cos(wt)*gP;
-    fu(2) = sin(wt)*gP;
+    fu(1+gN/2) = sin(wt)*gP;
 
     %激励力 0
-     ft(2) = -1/gQ;
+     ft(1+gN/2) = -1/gQ;
     %轴承力
     ts=1;
     fb(1)=ts*FxJSFun(dsp(1),dsp(2),vel(1),vel(2));
-    fb(2)=ts*FyJSFun(dsp(1),dsp(2),vel(1),vel(2));
+    fb(1+gN/2)=ts*FyJSFun(dsp(1),dsp(2),vel(1),vel(2));
     
     fd=fu+fb+ft;
 end
--- nfod/init_nfod.m
+++ nfod/init_nfod.m
@@ -1,18 +1,38 @@
 global gM gK gC gInvM gN gP gQ gW;
 gbearC = 0.025;
-n=1;
-rm = 70;
-rk = 0;
-rc = 0;
-gP = rm * 0.3;
-gQ = 0.1;
+n=6;
+rm = [1360 4123 1813 4123 2012 7123];
+rk = [0 1e9 1e9 1e9 1e9 1e9 0];
+rc = [0 0 0 0 0 0];
 
-gN=n*2;
-gM = [rm 0;0 rm];
+f1 = 1;
+w = 20;
+
+Mx = diag(rm);
+Mx =diag([70 1 1 1 1 1]);
+My = Mx;
+
+Kx = diag(rk(1:n))+diag(rk(2:n+1))-diag(rk(2:n),1)-diag(rk(2:n),-1);
+Kx = diag([0 0 0 0 0 0]);
+Ky = Kx;
+
+Cx = diag(rc);
+Cx =diag([0 0 0 0 0 0]);
+Cy = Cx;
+E6 = zeros(n);
+
+n=n*2;
+gN=n;
+
+gM = [Mx E6; E6 My];
 gInvM = inv(gM);
-gK = [rk 0;0 rk];
-gC = [rc 0;0 rc];
-gW = 1;
+gK = [Kx E6; E6 Ky];
+gC = [Cx E6; E6 Cy];
+
+gW = w;
+
+gP = 70 * 0.3;
+gQ = 0.1;
 
 dt = 0.01;
 nt = 6420/dt;
