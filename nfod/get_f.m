13e0436a4a37ca52458c4ec8534069ef933cd244
seacozz2007 <84089842@qq.com>
2017-06-07 16:47:37 +0800

    add 6 fod

--- nfod/get_f.m
+++ nfod/get_f.m
@@ -1,7 +1,7 @@
 function [ fd ] = get_f( wt,dsp,vel)
 %获取所受的力 包括 不平衡力Fu 激励力Ft 和 轴承力Fb
 %wt 当前转角,z 为位移
-    global gP gQ gN;
+    global gP gQ gN gS;
 
     fd = zeros(gN,1);
     fu = fd;
@@ -9,18 +9,25 @@ function [ fd ] = get_f( wt,dsp,vel)
     fb = fd;
     
     %不平衡力
-    xxi=1;
-    yyi=1+gN/2;
-    fu(xxi) = cos(wt)*gP;
-    fu(yyi) = sin(wt)*gP;
+    for i=[1 2 3 4 5]
+        xxi=i;
+        yyi=i+gN/2;
+        fu(xxi) = cos(wt)*gP;
+        fu(yyi) = sin(wt)*gP;
+    end
 
-    %激励力 0
-     ft(yyi) = -1/gQ;
+    %激励力 G
+    for i=[1 2 3 4 5 6]
+        yyi=i+gN/2;
+        ft(yyi) = -1*gQ;
+    end
     %轴承力
-    ts=1;
-    fb(xxi)=ts*FxJSFun(dsp(xxi),dsp(yyi),vel(xxi),vel(yyi));
-    fb(yyi)=ts*FyJSFun(dsp(xxi),dsp(yyi),vel(xxi),vel(yyi));
-    
+    for i=[1 3 5]
+        xxi=i;
+        yyi=i+gN/2;
+        fb(xxi)=gS*FxJSFun(dsp(xxi),dsp(yyi),vel(xxi),vel(yyi));
+        fb(yyi)=gS*FyJSFun(dsp(xxi),dsp(yyi),vel(xxi),vel(yyi));
+    end
     fd=fu+fb+ft;
 end
 
--- nfod/init_nfod.m
+++ nfod/init_nfod.m
@@ -1,18 +1,20 @@
-global gM gK gC gInvM gN gP gQ gW;
+global gM gK gC gInvM gN gP gQ gW gS;
 gbearC = 0.025;
 n=6;
 rm = [1360 4123 1813 4123 2012 7123];
 rk = [0 1e9 1e9 1e9 1e9 1e9 0];
+rk = [0 1 1 1 1 1 0]/1360;
 rc = [0 0 0 0 0 0];
 
 f1 = 1;
 w = 1;
 
 Mx = diag(rm);
-Mx =diag([70 1 1 1 1 1]);
+Mx =diag([70 70 70 70 70 70]/70);
+Mx =diag(rm/1360);
 My = Mx;
 
-Kx = diag(rk(1:n))+diag(rk(2:n+1))-diag(rk(2:n),1)-diag(rk(2:n),-1);
+Kx = diag(rk(1:n))+diag(rk(2:n+1))-diag(rk(2:n),1)-diag(rk(2:n),-1)/1360;
 Kx = diag([0 0 0 0 0 0]);
 Ky = Kx;
 
@@ -31,8 +33,9 @@ gC = [Cx E6; E6 Cy];
 
 gW = w;
 
-gP = 70 * 0.3;
-gQ = 0.1;
+gP = 70 * 0.3/70;
+gQ =1/ 0.1/70;
+gS = 1/70;
 
 dt = 0.01;
 nt = 6420/dt;
