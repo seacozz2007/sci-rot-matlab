a45aa08bb4f80da7ec008984db6a3b6cb5802dbc
eric.zhang <84089842@qq.com>
2017-06-26 13:42:45 +0800

    org

--- nfod/run_nfod_ffun.m
+++ nfod/run_nfod_ffun.m
@@ -4,4 +4,7 @@
     init_nfod_global;
     iniM=zeros(gN*2,1);
     [t,xy]=ode45(@ffun_nfod,0:dt:dt*nt,iniM,options);
-    n=2;
+    n=gdn;
+    for i=1:gdn
+        subplot(3,n,i+n*2); plot(t(ise),xy(ise,gsf(i)),'-');
+    end
